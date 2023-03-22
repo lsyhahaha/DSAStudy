55efb6d2613d907907164956c9bdf8ade0a0317d
SI_Xiaolong <sixiaolong2018@outlook.com>
2023-03-22 10:30:51 +0800

    顺序表按位删除实现。

--- LinearList/SequenceList/Static.cpp
+++ LinearList/SequenceList/Static.cpp
@@ -26,14 +26,31 @@ void InitList(SqList &L)
 // 基本操作——插入
 bool ListInsert(SqList &L, int i, int e)
 {
+    // 判断参数合法性
     if (i < 1 || i > L.length + 1)
         return false;
     if (L.length >= MaxSize)
         return false;
+    // 从最后一位开始，i之后的都想后移动一位
     for (int j = L.length; j >= i; j--)
         L.data[j] = L.data[j - 1];
     L.data[i - 1] = e;
     L.length++;
+    return true;
+}
+
+// 基本操作——删除
+bool ListDelete(SqList &L, int i, int &e)
+{
+    // 判断参数合法性
+    if (i < 1 || i > L.length)
+        return false;
+    // 将被删除的元素赋值给e
+    e = L.data[i - 1];
+    // 从i之后一位开始，i之后的都想前移动一位
+    for (int j = i; j < L.length; j++)
+        L.data[j - 1] = L.data[j];
+    L.length--;
     return true;
 }
 
@@ -46,6 +63,8 @@ int main()
     ListInsert(L, 2, 22);
     ListInsert(L, 3, 33);
 
+    printf("插入操作前:\n");
+
     // 违规打印整个顺序表
     for (int i = 0; i < MaxSize; i++)
         printf("data[%d]=%d\n", i, L.data[i]);
@@ -54,6 +73,17 @@ int main()
     ListInsert(L, 2, 3);
 
     printf("插入操作后:\n");
+
+    // 违规打印整个顺序表
+    for (int i = 0; i < MaxSize; i++)
+        printf("data[%d]=%d\n", i, L.data[i]);
+
+    // 用变量e把删除的元素“带回来”
+    int e = -1;
+    ListDelete(L, 2, e);
+
+    printf("删除操作后:\n");
+    printf("删除了元素%d\n", e);
 
     // 违规打印整个顺序表
     for (int i = 0; i < MaxSize; i++)
