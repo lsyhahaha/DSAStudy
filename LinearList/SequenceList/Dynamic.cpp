ab895c70ea2e89e5459a753baa481dacc6a76e1a
SI_Xiaolong <sixiaolong2018@outlook.com>
2023-03-22 10:43:33 +0800

    动态分配的插入和删除操作实现。

--- LinearList/SequenceList/Dynamic.cpp
+++ LinearList/SequenceList/Dynamic.cpp
@@ -25,6 +25,35 @@ void InitList(SeqList &L)
     L.MaxSize = InitSize;
 }
 
+// 基本操作——插入
+bool ListInsert(SeqList &L, int i, int e)
+{
+    // 判断参数合法性
+    if (i < 1 || i > L.length + 1)
+        return false;
+    // 从最后一位开始，i之后的都想后移动一位
+    for (int j = L.length; j >= i; j--)
+        L.data[j] = L.data[j - 1];
+    L.data[i - 1] = e;
+    L.length++;
+    return true;
+}
+
+// 基本操作——删除
+bool ListDelete(SeqList &L, int i, int &e)
+{
+    // 判断参数合法性
+    if (i < 1 || i > L.length)
+        return false;
+    // 将被删除的元素赋值给e
+    e = L.data[i - 1];
+    // 从i之后一位开始，i之后的都想前移动一位
+    for (int j = i; j < L.length; j++)
+        L.data[j - 1] = L.data[j];
+    L.length--;
+    return true;
+}
+
 // 增加动态数组的长度
 void IncreaseSize(SeqList &L, int len)
 {
@@ -44,7 +73,32 @@ int main()
     SeqList L;
     // 初始化顺序表
     InitList(L);
-    // 后续操作
+    // 在前三个位置写入数据
+    ListInsert(L, 1, 11);
+    ListInsert(L, 2, 22);
+    ListInsert(L, 3, 33);
+
+    printf("插入操作前:\n");
+
+    // 违规打印整个顺序表
+    for (int i = 0; i < InitSize; i++)
+        printf("data[%d]=%d\n", i, L.data[i]);
+
+    // 在第二个位置插入数据
+    ListInsert(L, 2, 3);
+
+    printf("插入操作后:\n");
+
+    // 违规打印整个顺序表
+    for (int i = 0; i < InitSize; i++)
+        printf("data[%d]=%d\n", i, L.data[i]);
+
+    // 用变量e把删除的元素“带回来”
+    int e = -1;
+    ListDelete(L, 2, e);
+
+    printf("删除操作后:\n");
+    printf("删除了元素%d\n", e);
 
     // 违规打印整个顺序表
     for (int i = 0; i < InitSize; i++)
