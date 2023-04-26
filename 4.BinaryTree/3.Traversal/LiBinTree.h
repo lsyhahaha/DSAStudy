ec9c13115cdc049f344f015136e69ba0473703f5
SI_Xiaolong <sixiaolong2018@outlook.com>
2023-04-26 16:11:22 +0800

    二叉树先中后序遍历实现。

---
+++ 4.BinaryTree/3.Traversal/LiBinTree.h
@@ -0,0 +1,55 @@
+﻿// 考研较多考
+
+#include <stddef.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <iostream>
+
+// 二叉链表
+typedef struct BiTNode
+{
+    // 数据域
+    int data;
+    // 左右子结点指针以及父节点指针
+    struct BiTNode *lchild, *rchild;
+} BiTNode, *BiTree;
+
+// 根节点插入数据并初始化
+bool InitTree(BiTree &root, int x)
+{
+    root = (BiTree)malloc(sizeof(BiTNode));
+    root->data = x;
+    root->lchild = NULL;
+    root->rchild = NULL;
+    return true;
+}
+
+// h表示插入左还是右
+bool InsertNode(BiTNode *f, int x, int h)
+{
+    BiTNode *c;
+    // 申请内存空间
+    c = (BiTNode *)malloc(sizeof(BiTNode));
+    // 插入数据
+    c->data = x;
+    // 左右子结点指针
+    c->lchild = NULL;
+    c->rchild = NULL;
+    if (f != c)
+    {
+        if (h == 0)
+        {
+            f->lchild = c;
+        }
+        else if (h == 1)
+        {
+            f->rchild = c;
+        }
+        else
+        {
+            std::cout << "变量 int h 填写有误，应为 0（代表左）或 1（代表右）" << std::endl;
+            return false;
+        }
+    }
+    return true;
+}
---
+++ 4.BinaryTree/3.Traversal/Traversal.cpp
@@ -0,0 +1,88 @@
+#include <stddef.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <iostream>
+#include "LiBinTree.h"
+
+void visit(BiTNode *T)
+{
+    std::cout << "当前结点数据：" << T->data << std::endl;
+}
+
+// 先序遍历操作过程
+// 1、若二叉树为空，则什么也不做
+// 2、若二叉树非空
+//   1) 访问根节点
+//   2) 先序遍历左子树
+//   3) 先序遍历右子树
+void PerOrder(BiTree T)
+{
+    if (T != NULL)
+    {
+        // 访问根节点
+        visit(T);
+        // 递归遍历左子树
+        PerOrder(T->lchild);
+        // 递归遍历右子树
+        PerOrder(T->rchild);
+    }
+}
+
+// 中序遍历操作过程
+// 1、若二叉树为空，则什么也不做
+// 2、若二叉树非空
+//   1) 中序遍历左子树
+//   2) 访问根节点
+//   3) 中序遍历右子树
+void InOrder(BiTree T)
+{
+    if (T != NULL)
+    {
+        InOrder(T->lchild);
+        visit(T);
+        InOrder(T->rchild);
+    }
+}
+
+// 后序遍历操作过程
+// 1、若二叉树为空，则什么也不做
+// 2、若二叉树非空
+//   1) 后序遍历左子树
+//   2) 后序遍历右子树
+//   3) 访问根节点
+void PostOrder(BiTree T)
+{
+    if (T != NULL)
+    {
+        PostOrder(T->lchild);
+        PostOrder(T->rchild);
+        visit(T);
+    }
+}
+
+int main()
+{
+    // 定义一颗空树
+    BiTree root = NULL;
+    InitTree(root, 1);
+
+    InsertNode(root, 2, 0);
+    InsertNode(root, 3, 1);
+    InsertNode(root->lchild, 4, 0);
+    InsertNode(root->lchild, 5, 1);
+
+    std::cout << "根节点：" << root->data << std::endl
+              << "根节点的左节点：" << root->lchild->data << std::endl
+              << "根节点的右节点：" << root->rchild->data << std::endl
+              << "根节点的左节点的左节点：" << root->lchild->lchild->data << std::endl
+              << "根节点的左节点的右节点：" << root->lchild->rchild->data << std::endl;
+
+    std::cout << "开始先序遍历！" << std::endl;
+    PerOrder(root);
+    std::cout << "开始中序遍历！" << std::endl;
+    InOrder(root);
+    std::cout << "开始后序遍历！" << std::endl;
+    PostOrder(root);
+
+    return 0;
+}
