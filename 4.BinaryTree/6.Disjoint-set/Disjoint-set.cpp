f784da2436378fd8cc3f861f2a28f8066cf7e692
SI_Xiaolong <sixiaolong2018@outlook.com>
2023-05-16 09:07:46 +0800

    并查集的进一步优化。

--- 4.BinaryTree/6.Disjoint-set/Disjoint-set.cpp
+++ 4.BinaryTree/6.Disjoint-set/Disjoint-set.cpp
@@ -14,7 +14,7 @@ void Inital(int S[])
 
 // 查操作（时间复杂度O(n)），找x所属集合（返回x所属根节点）
 // 复杂度中的n，实际上就是树的高度h，优化策略就是让树不要长太高，
-// 也就是并操作时，尽量让小树并到大树里
+// 也就是并操作时，尽量让小树并到大树里，此时时间复杂度为O(log₂n)
 int Find(int S[], int x)
 {
     // 循环寻找x的根
@@ -62,11 +62,33 @@ bool Union2(int S[], int Root1, int Root2)
     return true;
 }
 
+// 更进一步的优化：Find操作的优化（压缩路径）
+// 每次Find操作，先找到根，再进行压缩。可以使树的高度不超过O(a(n))。
+// a(n)是一个增长很欢吗的函数，对于常见的n，通常a(n)<=4，即通常O(a(n))=O(1)。
+int Find2(int S[], int x)
+{
+    // 先找到根节点，再进行路径压缩
+    int root = Find(S, x);
+    // 压缩路径
+    while (x != root)
+    {
+        // t指向x的父结点
+        int t = S[x];
+        // x直接挂到根节点下
+        S[x] = root;
+        x = t;
+    }
+    return root;
+}
+
 int main()
 {
-    std::cout << "查找第六个位置的根：" << Find(UFSets, 6) << std::endl;
+    std::cout << "查找第6个位置的根：" << Find(UFSets, 6) << std::endl;
     Union2(UFSets, 0, 2);
-    std::cout << "并操作后，查找第六个位置的根：" << Find(UFSets, 6) << std::endl;
+    std::cout << "并操作后，查找第6个位置的根：" << Find(UFSets, 6) << std::endl;
+    std::cout << "查找第10个位置的根：" << Find(UFSets, 6) << std::endl;
+    std::cout << "优化查找第10个位置的根：" << Find2(UFSets, 6) << std::endl;
+    
 
     return 0;
 }
